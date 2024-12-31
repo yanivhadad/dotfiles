d7574b33819f961532de29c37ac5c80b0327d5b4
Yaniv Hadad <yaniv.hadad@similarweb.com>
2024-12-31 11:43:14 +0200

    cleanup

--- init.sh
+++ init.sh
@@ -1,14 +1,4 @@
-
-# install oh my zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# install nvm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
 curl https://get.volta.sh | bash
-
-rm -rf ~/.oh-my-zsh
-rm -rf ~/.nvm
