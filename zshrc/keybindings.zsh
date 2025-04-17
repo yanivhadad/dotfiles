ce25149748b9da5dfeb5f8944886468717fb9436
Yaniv Hadad <yaniv@similartech.com>
2025-04-17 12:17:50 +0300

    add ctrl+o to open folder in vscode

--- zshrc/keybindings.zsh
+++ zshrc/keybindings.zsh
@@ -1,3 +1,24 @@
+fzf-code-widget() {
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local dir="$(
+    FZF_DEFAULT_COMMAND=${FZF_ALT_C_COMMAND:-} \
+    FZF_DEFAULT_OPTS=$(__fzf_defaults "--reverse --walker=dir,follow,hidden --scheme=path" "${FZF_ALT_C_OPTS-} +m") \
+    FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd) < /dev/tty)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  zle push-line # Clear buffer. Auto-restored on next prompt.
+  BUFFER="code ${(q)dir:a}"
+  zle accept-line
+  local ret=$?
+  unset dir # ensure this doesn't end up appearing in prompt expansion
+  zle reset-prompt
+  return $ret
+}
+zle -N fzf-code-widget
+
 bindkey '^E' fzf-cd-widget
+bindkey '^O' fzf-code-widget
 bindkey '^I^I' autosuggest-accept
 bindkey jj vi-cmd-mode
